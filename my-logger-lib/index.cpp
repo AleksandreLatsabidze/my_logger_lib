ba99ea18258a392421c02f25dd16d9d633599947
AleksandreLatsabidze <aleksandrelatsabidze5@gmail.com>
2025-04-10 00:45:55 +0400

    Added feature for logging levels

---
+++ my-logger-lib/index.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+using namespace std;
+enum LoggingLevel
+{
+    DEBUG,
+    INFO,
+    ERROR,
+};
+class ConsoleLogger
+{
+public:
+    void print(LoggingLevel level, string message) const
+    {
+        if (message.empty())
+        {
+            throw invalid_argument("It cannot be empty");
+        }
+        if (message == "null")
+        {
+            throw invalid_argument("Cannot be 'null'");
+        }
+
+        string levelStr;
+        switch (level)
+        {
+        case DEBUG:
+            levelStr = "DEBUG";
+            break;
+        case INFO:
+            levelStr = "INFO";
+            break;
+        case ERROR:
+            levelStr = "ERROR";
+            break;
+        default:
+            throw invalid_argument("Invalid logging level.");
+        }
+
+        cout << "[" << levelStr << "]: " << message << endl;
+    }
+};
+bool isValidPhoneNumber(const string& phone)
+{
+    for (char c : phone) {
+        if (!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main()
+{
+    ConsoleLogger logger;
+    try
+    {
+        logger.print(DEBUG, "Application started");
+        logger.print(INFO, "Application finished");
+        logger.print(ERROR, "Application error");
+        logger.print(DEBUG, ""); 
+    
+        string phoneNumber;
+        cout << "Enter phone number (only digits allowed): ";
+        getline(cin, phoneNumber);
+
+        // Validate phone number (only digits allowed)
+        while (!isValidPhoneNumber(phoneNumber))
+        {
+            cout << "Invalid phone number. Only digits are allowed. Please enter a valid phone number: ";
+            getline(cin, phoneNumber);
+        }
+ 
+        cout << "Entered phone number: " << phoneNumber << endl;
+        
+    }
+    catch (const exception &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+ 
+  
+    string phoneNumber;
+    cout << "Enter phone number again (only digits allowed): ";
+    getline(cin, phoneNumber);
+
+    while (!isValidPhoneNumber(phoneNumber))
+    {
+        cout << "Invalid phone number. Only digits are allowed. Please enter a valid phone number: ";
+        getline(cin, phoneNumber);
+    }
+
+    cout << "Entered phone number: " << phoneNumber << endl;
+
+    return 0;
+}
